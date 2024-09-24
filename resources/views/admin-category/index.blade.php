d9e7e38555818253e683ca9e8ab2b348e28020d1
Jonathan Betancur <jonathanbetancurespinosa@gmail.com>
2024-09-24 16:46:53 -0500

    admin categories views and actions

--- resources/views/admin-category/index.blade.php
+++ resources/views/admin-category/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title', 'Home Page - Online Store')
+@section('title', 'Categories - Admin')
 @section('content')
 
 <div class="container mt-4">
@@ -10,26 +10,39 @@
   @endif
   <div class="row">
     <div class="col-12">
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach ($viewData["categories"] as $category)
-          <tr>
-            <td>{{ $category["id"] }}</td>
-            <td>
-              <a href="{{ route('admin-category.show', ['id'=> $category["id"]]) }}" class="text-decoration-none">
-                {{ $category["name"] }}
-              </a>
-            </td>
-          </tr>
-          @endforeach
-        </tbody>
-      </table>
+      <div class="card">
+        <div class="card-header">
+          <h3>Categories</h3>
+        </div>
+        <div class="card-body">
+          <table class="table table-striped table-hover">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col"># of Games</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach ($viewData["categories"] as $category)
+              <tr>
+                <td>{{ $category->getId() }}</td>
+                <td>
+                  <a href="{{ route('admin-category.show', ['id'=> $category->getId()]) }}" class="text-decoration-none">
+                    {{ $category->getName() }}
+                  </a>
+                </td>
+                <td>
+                  <span>
+                    {{ $category->getGames()->count() }}
+                  </span>
+                </td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   </div>
 </div>
--- resources/views/admin-category/show.blade.php
+++ resources/views/admin-category/show.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title', $viewData['category']['name'].' - Online Store')
+@section('title', $viewData['category']->getName().' - Online Store')
 @section('content')
 
 <div class="container mt-4">
@@ -8,31 +8,37 @@
       {{ session('success') }}
     </div>
   @endif
-  <table class="table table-bordered">
-    <thead>
-      <tr>
-        <th scope="col">ID</th>
-        <th scope="col">Name</th>
-        <th scope="col">Description</th>
-        <th scope="col">Actions</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>{{ $viewData["category"]["id"] }}</td>
-        <td>{{ $viewData["category"]["name"] }}</td>
-        <td>{{ $viewData["category"]["description"] }}</td>
-        <td>
-          <a href="" class="btn btn-primary">Edit</a>
-          <form action="{{ route('admin-category.destroy', ['id' => $viewData['category']['id']]) }}" method="POST" style="display:inline;">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn btn-danger">Delete</button>
-          </form>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="card">
+    <div class="card-header">
+      <h3>Category Details</h3>
+    </div>
+    <div class="card-body">
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{{ $viewData['category']->getId() }}</td>
+            <td>{{ $viewData['category']->getName() }}</td>
+            <td>{{ $viewData['category']->getDescription() }}</td>
+            <td>
+              <form action="{{ route('admin-category.destroy', ['id' => $viewData['category']->getId()]) }}" method="POST" style="display:inline;">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">Delete</button>
+              </form>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </div>
 
 @endsection
