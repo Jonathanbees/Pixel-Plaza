50d226058f951ab73852ae5d1f1f895f8f7005a2
Jonathan Betancur <jonathanbetancurespinosa@gmail.com>
2024-09-24 16:52:55 -0500

    category admin show style

--- resources/views/admin-category/show.blade.php
+++ resources/views/admin-category/show.blade.php
@@ -1,44 +1,29 @@
 @extends('layouts.app')
+
 @section('title', $viewData['category']->getName().' - Online Store')
+
 @section('content')
-
-<div class="container mt-4">
-  @if(session('success'))
-    <div class="alert alert-success">
-      {{ session('success') }}
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header text-center">
+                    <h4>Category Details</h4>
+                </div>
+                <div class="card-body">
+                    <p><strong>ID:</strong> {{ $viewData['category']->getId() }}</p>
+                    <p><strong>Name:</strong> {{ $viewData['category']->getName() }}</p>
+                    <p><strong>Description:</strong> {{ $viewData['category']->getDescription() }}</p>
+                </div>
+                <div class="card-footer text-center">
+                    <form action="{{ route('admin-category.destroy', ['id'=> $viewData['category']->getId()]) }}" method="POST" class="d-inline">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-  @endif
-  <div class="card">
-    <div class="card-header">
-      <h3>Category Details</h3>
-    </div>
-    <div class="card-body">
-      <table class="table table-bordered">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Name</th>
-            <th scope="col">Description</th>
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{{ $viewData['category']->getId() }}</td>
-            <td>{{ $viewData['category']->getName() }}</td>
-            <td>{{ $viewData['category']->getDescription() }}</td>
-            <td>
-              <form action="{{ route('admin-category.destroy', ['id' => $viewData['category']->getId()]) }}" method="POST" style="display:inline;">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-danger">Delete</button>
-              </form>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
 </div>
-
 @endsection
